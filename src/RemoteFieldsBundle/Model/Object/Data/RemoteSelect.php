3882cf376e356a6d9bcb272501030d2a35c1457e
triputvp <slava.triput@gmail.com>
2021-09-29 11:38:51 +0300

    initial commit

--- src/RemoteFieldsBundle/Model/Object/Data/RemoteSelect.php
+++ src/RemoteFieldsBundle/Model/Object/Data/RemoteSelect.php
@@ -8,4 +8,15 @@ class RemoteSelect extends Select{
 
     public $fieldtype = "RemoteSelect";
 
+    public $remoteStorage = null;
+
+
+    public function getRemoteStorage(){
+        return $this->remoteStorage;
+    }
+
+    public function setRemoteStorage($remoteStorage){
+        $this->remoteStorage = $remoteStorage;
+        return $this;
+    }
 }
