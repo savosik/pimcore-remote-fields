411de48c011a8a6d1ad531f543a19524d33e251f
slava <slava.triput@gmail.com>
2021-10-07 20:42:48 +0300

    remote select works perfect

--- src/RemoteFieldsBundle/Resources/public/admin/js/classfields/remote-multiselect/tag.js
+++ src/RemoteFieldsBundle/Resources/public/admin/js/classfields/remote-multiselect/tag.js
@@ -1,20 +1,7 @@
-/**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Commercial License (PCL)
- * Full copyright and license information is available in
- * LICENSE.md which is distributed with this source code.
- *
- * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- * @license    http://www.pimcore.org/license     GPLv3 and PCL
- */
+pimcore.registerNS("pimcore.object.tags.remoteMultiSelect");
+pimcore.object.tags.remoteMultiSelect = Class.create(pimcore.object.tags.abstract, {
 
-pimcore.registerNS("pimcore.object.tags.remoteMultiselect");
-pimcore.object.tags.remoteMultiselect = Class.create(pimcore.object.tags.abstract, {
-
-    type: "remoteMultiselect",
+    type: "remoteMultiSelect",
     allowBatchAppend: true,
     allowBatchRemove: true,
 
