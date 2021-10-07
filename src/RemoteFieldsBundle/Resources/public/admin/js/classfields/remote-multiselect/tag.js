509ad4f81399a41f7afb9501e6e83531b60f96b3
slava <slava.triput@gmail.com>
2021-10-07 21:25:05 +0300

    remote select works perfect

--- src/RemoteFieldsBundle/Resources/public/admin/js/classfields/remote-multiselect/tag.js
+++ src/RemoteFieldsBundle/Resources/public/admin/js/classfields/remote-multiselect/tag.js
@@ -150,7 +150,7 @@ pimcore.object.tags.remoteMultiSelect = Class.create(pimcore.object.tags.abstrac
             getValue: function (){
                 var curr_values = this.value;
                 var curr_labels = this.getRawValue();
-                var result = null;
+                var result = [];
                 if(curr_values.length > 0){
                     var labels = curr_labels.split(",");
 
